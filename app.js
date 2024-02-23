ce7e3b021c1b8b09062bf566ed3869367d5092b9
Abrar Ahmed <appdev1990w@gmail.com>
2024-02-22 17:29:13 -0800

    JS Alert

---
+++ app.js
@@ -0,0 +1,107 @@
+// <-------Chapter 1------>
+
+// Q1
+// alert("Hello World")
+
+// Q2
+// alert("Error! Please enter a valid password.")
+
+// Q3
+// alert("Welcome to JS Land...\nHappy Coding!")
+
+// Q4
+// alert("Welcome to JS Land...")
+
+// Q5
+// alert("Happy Coding!\nPrevnet this page from creating additional dialogs.")
+
+// Q6
+// alert("Hello... I can run JS through my web browser's console")
+
+// <-------Chapter 2------>
+
+// Q1
+
+//  var username = ("Johne Doe")
+
+//  alert(username)
+
+// Q2
+
+// var myName = ("Johne Doe")
+
+// alert("Full Name "+ myName)
+
+// Q3
+
+// var massage = ("Hello World")
+
+// alert(massage)
+
+// Q4
+
+// var FullName = ("Johne Doe")
+
+// var age = (15)
+
+// var studentsbio = ("Certified Mobile Application Development")
+
+// alert(FullName)
+
+// alert(age+" years old")
+
+// alert(studentsbio)
+
+// Q5
+
+// var a=("PIZZA") , b=("PIZZ") , c=("PIZ") , d=("PI") , e=("P")
+
+// alert(a+"\n"+b+"\n"+c+"\n"+d+"\n"+e)
+
+// Q6
+
+// var email=("example@gmail.com")
+
+// alert(email)
+
+// Q7
+
+// var book=("A smarter way to learn to JavaScript")
+
+// alert("I am trying to learn from the Book "+book)
+
+// Q8
+
+// document.write("<h1>Yah! I can write HTML content through JavaScript</h1>")
+
+// Q9
+
+// var a=("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+// alert(a)
+
+// <-------Chapter 3------>
+
+// Q1
+
+// var age=(15)
+
+// alert("I am "+age+" years old")
+
+// Q2
+
+// var N=(14)
+
+// alert("You have visited this site "+N+" times")
+
+// Q3
+
+// var birthYear=(1990)
+
+// document.write("My birth year is ",birthYear,"<br>Data type of my ddeclared variable is number")
+
+// Q4
+
+// var visiterName=("John Doe") , Product_title=("T-shirt") , Quantity=(5)
+
+// document.write(visiterName," ordered ",Quantity,Product_title,"(s) on XYZ Clothing store")
